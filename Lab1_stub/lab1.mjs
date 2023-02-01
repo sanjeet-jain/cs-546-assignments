b59038a7e3d05b2216c073cdefbd9954a3acfa2c
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-02-01 16:56:28 -0500

    solution 1 2 3

--- Lab1_stub/lab1.mjs
+++ Lab1_stub/lab1.mjs
@@ -1,25 +1,63 @@
 export const questionOne = (arr) => {
   // Implement question 1 here
-  return; //return result
+  // using reduce function to calculate the cube of each element and add it to the initial value of 0
+  //Reference : https://www.w3schools.com/jsref/jsref_reduce.asp
+  let sum = arr.reduce(
+    (previousValue, currentValue) => previousValue + Math.pow(currentValue, 3),
+    0
+  );
+  //loop through numbers 2 to the the sum-1 and check if its divisible by any number other than itself and 1
+  for (let i = 2; i < sum; i++) {
+    if (sum % i == 0) {
+      return { [sum]: false };
+    }
+  }
+  return { [sum]: true };
 };
 
 export const questionTwo = (numArray) => {
   // Implement question 2 here
-  return; //return result
+  for (let i = 0; i < numArray.length - 1; i++) {
+    if (numArray[i] > numArray[i + 1]) {
+      // since we have found first occurence we return indices and false flag
+      return [i, i + 1, false];
+    }
+  }
+  // no occurence found so its sorted
+  return [true];
 };
 
 export const questionThree = (obj1, obj2) => {
   // Implement question 3 here
-  return; //return result
+
+  let arrA = Object.keys(obj1);
+  let arrB = Object.keys(obj2);
+  // we find the common keys of both key lists
+  // then we find the uncommon ones
+  // we convert both lists to object of format key:true or key:false and then combine them
+  let result = new Object();
+  arrA
+    .filter((x) => arrB.includes(x))
+    .forEach((element) => {
+      result[element] = true;
+    });
+  arrA
+    .filter((x) => !arrB.includes(x))
+    .concat(arrB.filter((x) => !arrA.includes(x)))
+    .forEach((element) => {
+      result[element] = false;
+    });
+  return result;
 };
 
 export const questionFour = (string) => {
   // Implement question 4 here
+
   return; //return result
 };
 
 export const studentInfo = {
-  firstName: "YOUR FIRST NAME",
-  lastName: "YOUR LAST NAME",
-  studentId: "YOUR STUDENT ID",
+  firstName: "Sanjeet Vinod",
+  lastName: "Jain",
+  studentId: "20012768",
 };
--- Lab1_stub/lab1.test.mjs
+++ Lab1_stub/lab1.test.mjs
@@ -1,3 +1,42 @@
 import * as lab1 from "./lab1.mjs";
-
 //TODO: Write and call each function in lab1.js 5 times each, passing in different input
+// Assumption : we are passing only valid inputs to all functions
+
+// testing question 1
+console.log(lab1.questionOne([])); // returns { '0': true }
+console.log(lab1.questionOne([0, 0])); // returns { '0': true }
+console.log(lab1.questionOne([1, 2, 0, 10, 9])); // returns { '1738': false }
+console.log(lab1.questionOne([5, 3, 10, 11, 99, 123124, 1245125, 1253])); // returns { '1932228945368172800': false }
+console.log(lab1.questionOne([214121, 12425, 2345, 1234, 124321])); // returns { '1.932921821821283e+21': false }
+console.log();
+
+// testing question 2
+console.log(lab1.questionTwo([])); // Returns and then outputs [true]
+console.log(lab1.questionTwo([1])); // Returns and then outputs [true]
+console.log(lab1.questionTwo([1, 0])); // Returns and then outputs [true]
+console.log(lab1.questionTwo([1, 2, 3, 4, 5, 6, 7])); // Returns and then outputs [true]
+console.log(lab1.questionTwo([1, 2, 3, 4, 8, 6, 0, 9, 0, 11])); // Returns and then outputs [ 4, 5, false ]
+console.log(lab1.questionTwo([0, 1, 2, 11, 12, 111, 0])); // Returns and then outputs [ 5,6, false ]
+console.log();
+
+// testing question 3
+console.log(lab1.questionThree({ a: 1, b: 2, c: 3 }, { c: 10, a: 20, b: 30 })); // Returns and then outputs {a:true, b:true, c:true}
+console.log(
+  lab1.questionThree(
+    { a: 1, b: 2, c: 3, d: 4 },
+    { g: 10, b: 20, e: 30, d: 40, c: 50, a: 60 }
+  )
+); // Returns and then outputs {a:true, b:true, c:true, d:true, e:false, f:false}
+console.log(lab1.questionThree({ g: 1, 1: 2, c: 3 }, { c: 10, a: 20, 1: 30 })); // Returns and then outputs { '1': true, c: true, g: false, a: false }
+console.log(lab1.questionThree({ a: 1, b: 2, c: 3 }, {})); // Returns and then outputs { a: false, b: false, c: false }
+console.log(lab1.questionThree({ 1: 1 }, {})); // Returns and then outputs { '1': false }
+console.log();
+
+//testing question 4
+console.log(
+  lab1.questionFour(`Patrick,Hill,cs546
+Jared,Bass,cs115
+Shudong,Hao,cs570`)
+);
+//should return and then log [["Patrick", "Hill", "cs546"],["Jared", "Bass", "cs115"], ["Shudong", "Hao", "cs570"]]
+console.log();
