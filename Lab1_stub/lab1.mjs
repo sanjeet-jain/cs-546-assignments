c5d2320693cecbc3f4d1a452a372eace68aec158
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-02-01 16:56:09 -0500

    initial

---
+++ Lab1_stub/lab1.mjs
@@ -0,0 +1,25 @@
+export const questionOne = (arr) => {
+  // Implement question 1 here
+  return; //return result
+};
+
+export const questionTwo = (numArray) => {
+  // Implement question 2 here
+  return; //return result
+};
+
+export const questionThree = (obj1, obj2) => {
+  // Implement question 3 here
+  return; //return result
+};
+
+export const questionFour = (string) => {
+  // Implement question 4 here
+  return; //return result
+};
+
+export const studentInfo = {
+  firstName: "YOUR FIRST NAME",
+  lastName: "YOUR LAST NAME",
+  studentId: "YOUR STUDENT ID",
+};
---
+++ Lab1_stub/lab1.test.mjs
@@ -0,0 +1,3 @@
+import * as lab1 from "./lab1.mjs";
+
+//TODO: Write and call each function in lab1.js 5 times each, passing in different input
