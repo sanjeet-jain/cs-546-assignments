f1b341695388eed45a6ac49e2d8e578464c4e72d
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-02-09 16:04:02 -0500

    question 1

--- lab2_stub-1/arrayUtils.js
+++ lab2_stub-1/arrayUtils.js
@@ -1,15 +1,22 @@
-/* Todo: Implment the functions below and then export them
+/* TODO: Implment the functions below and then export them
       using the ES6 exports syntax. 
       DO NOT CHANGE THE FUNCTION NAMES
 */
+import * as helper from "./helpers.js";
+export let sortAndFilter = (
+  array,
+  sortBy1,
+  sortBy2,
+  filterBy,
+  filterByTerm
+) => {
+  helper.validateInputForSortAndFilter(array);
+};
 
-let sortAndFilter = (array, sortBy1, sortBy2, filterBy, filterByTerm) => {};
-
-let merge = (...args) => {
+export let merge = (...args) => {
   //this function takes in a variable number of arrays that's what the ...args signifies
 };
 
-let matrixMultiply = (...args) => {
-    //this function takes in a variable number of arrays that's what the ...args signifies
+export let matrixMultiply = (...args) => {
+  //this function takes in a variable number of arrays that's what the ...args signifies
 };
-
--- lab2_stub-1/helpers.js
+++ lab2_stub-1/helpers.js
@@ -1,3 +1,44 @@
-/* Todo: Implment any helper functions below 
+/* TODO: Implment any helper functions below 
     and then export them for use in your other files.
 */
+
+function isNull(input) {
+  if (isNaN(input) || input === undefined || input === null) {
+    return true;
+  }
+  return false;
+}
+function isEmpty(input) {
+  if (input === [] || input === "" || Object.keys(input).length === 0) {
+    return true;
+  }
+
+  return false;
+}
+
+export function validateInputForSortAndFilter(inputValue) {
+  let isValid = false;
+  let errorMessage = "Error: ";
+  if (isNull(inputValue) || isEmpty(inputValue)) {
+    isValid = false;
+    errorMessage += "the input array is null or empty!";
+  }
+
+  if (!isValid) {
+    throw errorMessage;
+  }
+}
+
+// export function validateInputForMerge(inputValue){
+//     let isValid = False;
+//     if(inputValue)
+
+//     return isValid
+// }
+
+// export function validateInputForMatrixMultiply(inputValue){
+//     let isValid = False;
+//     if(inputValue)
+
+//     return isValid
+// }
