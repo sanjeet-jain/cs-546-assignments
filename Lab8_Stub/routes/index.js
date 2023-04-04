cffb6620297cd206b6193bb48f86e4fee9d7ff10
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-04-04 15:50:56 -0400

    lab 8 added

---
+++ Lab8_Stub/routes/index.js
@@ -0,0 +1,1 @@
+//Here you will import route files and export them as used in previous labs
---
+++ Lab8_Stub/routes/venues.js
@@ -0,0 +1,13 @@
+//Import express and express router as shown in lecture code and worked in previous labs
+//You can make your axios calls to the API directly in the routes
+router.route('/').get(async (req, res) => {
+  //code here for GET
+});
+
+router.route('/searchvenues').post(async (req, res) => {
+  //code here for POST
+});
+
+router.route('/venuedetails/:id').get(async (req, res) => {
+  //code here for GET
+});
